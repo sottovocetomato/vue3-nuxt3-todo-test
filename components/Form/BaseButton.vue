17581b6f698b92e858b9b397641c96f38c50e2ad
ai-strings <adrstrings@gmail.com>
2024-12-21 15:38:01 +0300

    !wip undo redo

--- components/Form/BaseButton.vue
+++ components/Form/BaseButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    :id="uid"
     :class="[
       'btn',
       variant ? `btn-${variant}` : '',
@@ -7,21 +8,25 @@
       customClass,
     ]"
     :disabled="disabled"
+    :type="type"
   >
     <slot></slot>
   </button>
 </template>
 
 <script setup>
+const uid = useId();
 const {
   variant = "",
   disabled = false,
   text = "",
+  type = "button",
   size = "",
   customClass = "",
 } = defineProps({
   variant: String,
   size: String,
+  type: String,
   customClass: String,
   disabled: Boolean,
   text: Boolean,
